af2f28e8439634742606d145efe4e47faf67023b
SSto <95592688+sstover25@users.noreply.github.com>
2022-02-26 15:23:48 -0600

    finalized README template and completed license functionalities

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,7 +33,13 @@ const licenseArr = [
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license) {
-    return `[![${license.name}](https://img.shields.io/badge/${license.img})](https://opensource.org/licenses/${license.url})`;
+    for (i = 0; i < licenseArr.length; i++) {
+      const chosenLicense = licenseArr[i];
+      if (license === chosenLicense.name) {
+        return `
+[![License](https://img.shields.io/badge/${chosenLicense.img})](https://opensource.org/licenses/${chosenLicense.url})`;
+      }
+    }
   }
   return "";
 }
@@ -42,64 +48,53 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license) {
-    return `- [License](#license) <br />`;
+    return `[License](#license) <br />`;
   }
   return "";
 }
-//${renderLicenseBadge(licenseArr[i])}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license) {
     for (i = 0; i < licenseArr.length; i++) {
-      let licenseName = licenseArr[i].name;
+      const licenseName = licenseArr[i].name;
       if (license === licenseName) {
         return `## License
 
-      ${renderLicenseBadge(licenseArr[i])}
-        `;
+      This project is covered under the ${licenseName}.`;
       }
     }
-    return "";
   }
+  return "";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # **${data.title}**
-
+  ${renderLicenseBadge(data.license)}
   ## Description
 
         ${data.description}
-
   ## Table of Contents
-  - [Installation](#installation) <br />
-  - [Usage](#usage) <br />
-  ${renderLicenseLink(data.license)}
-  - [Contributing](#contributing) <br />
-  - [Tests](#tests) <br />
-  - [Questions](#questions)
-
+  [Installation](#installation) <br /> [Usage](#usage) <br /> ${renderLicenseLink(
+    data.license
+  )}[Contributing](#contributing) <br /> [Tests](#tests) <br /> [Questions](#questions)
 
   ## Installation
 
         ${data.installation}
-
   ## Usage
 
         ${data.usage}
-
   ${renderLicenseSection(data.license)}
-
   ## Contributing
 
         ${data.contribution}
-
   ## Tests
 
         ${data.test}
-
   ## Questions
 
   If any additional questions arise, you can find me at [GitHub](https://github.com/${
