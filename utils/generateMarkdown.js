157616e50337f22795dbedd99bffced301f277d3
SSto <95592688+sstover25@users.noreply.github.com>
2022-02-26 13:00:07 -0600

    added additional styling for README template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,46 +2,64 @@ const fs = require("fs");
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title}
+  # **${data.title}**
 
   ## Description
+
   ${data.description}
 
   ## Table of Contents
-  -[Installation](#installation) <br />
-  -[Usage](#usage) <br />
-  -[License](#license) <br />
-  -[Contributing](#contributing) <br />
-  -[Tests](#tests) <br />
-  -[Questions](#questions)
+  - [Installation](#installation) <br />
+  - [Usage](#usage) <br />
+  - [License](#license) <br />
+  - [Contributing](#contributing) <br />
+  - [Tests](#tests) <br />
+  - [Questions](#questions)
 
 
   ## Installation
+
   ${data.installation}
 
   ## Usage
+
   ${data.usage}
 
   ## License
+
   ${data.license}
 
   ## Contributing
+
   ${data.contribution}
 
   ## Tests
+
   ${data.test}
 
   ## Questions
